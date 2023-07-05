89cdf8a231e58f3dce07e7506aa7b4e9ad3df35d
purvikkotecha123 <87319354+purvikkotecha123@users.noreply.github.com>
2023-07-05 17:26:31 +0530

    Update index.js static

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 
-const express = require("express");
-const session = require('express-session');
-const https = require('https');
-const { stringify } = require("querystring");
-const cors = require('cors');
-
-const PORT = process.env.PORT || 3000;
-
-
-
-const app = express();
-app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
-app.use(express.urlencoded({
-  extended: true
-}));
-
-let sess;
-app.get('/', (req, res) => {
-  sess = req.session;
-  sess.order_id;
-  res.sendFile(__dirname +  '/index.html');
-});
 /*
  * Define the version of the Google Pay API referenced when creating your
  * configuration
@@ -175,4 +153,3 @@ const baseRequest = {
     }
   }
     
-  app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
